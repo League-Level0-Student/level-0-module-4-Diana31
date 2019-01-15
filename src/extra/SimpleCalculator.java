fdca83514cbfe6b7774a69298922f7d3a8f044cc
538751 <538751@0338S-S-R0B6H0F.sdcs.local>
2019-01-15 14:22:28 -0800

    string method complete, simple calculator

--- src/extra/SimpleCalculator.java
+++ src/extra/SimpleCalculator.java
@@ -14,7 +14,18 @@ public class SimpleCalculator {
 		int operation = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
 				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Add ", "Subtract ", "Multiply ", "Divide " },
 				null);
-
+	if (operation==0) {
+		add(number1, number2); 
+	}
+	else if (operation==1) {
+		subtract(number1, number2);
+	}
+	else if (operation==2) {
+		multiply(number1, number2);
+	}
+	else if (operation==3) {
+		divide(number1, number2);
+	}
 		// 5. Call the methods created in steps 3 and 4 to perform the appropriate operation.
 
 		// 6. Wrap steps 1, 2 and 5 in a for loop to repeat calculator operations
@@ -23,6 +34,8 @@ public class SimpleCalculator {
 	// 3. Create method for addition operation.
 	//    Method should create pop-up that shows equation and solution, eg. 45 + 25 = 70.
 	//    HINT: use 'static void add(int num1, num2) { ... }
-	
+	static void add(number1, number2) {
+		JOptionPane.showMessageDialog(null,""+num);
+	}
 	// 4. Create similar methods for subtraction, multiplication and division.
 }
